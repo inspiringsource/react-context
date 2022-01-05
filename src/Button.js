7d311faf27048dced40a02a40c1c5c43828f574f
inspiringsource <ab.b2k3@gmail.com>
2022-01-05 13:50:02 +0100

    init commit

---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ThemeContext from "./themeContext"
+
+function Button(props) {
+    return (
+        <ThemeContext.Consumer>
+            {
+                theme => (
+                    <button className={`${theme}-theme`}>Switch Theme</button>
+                )}
+        </ThemeContext.Consumer>
+    )    
+}
+
+export default Button
